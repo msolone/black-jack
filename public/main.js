6041b6ca1824c6ac03a321f2a4e2a8fc51c3a7d5
Michael Solone <michaelsolone@gmail.com>
2018-08-02 14:39:22 -0400

    Deck is shuffled

--- public/main.js
+++ public/main.js
@@ -10,7 +10,7 @@ const suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 const rank = ['Ace', '2','3','4','5','6','7','8','9','10','Jack','Queen','King']
 const cardValue = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-
+// Creates an Array of 52 objects with rank, suits, and card values
 const createDeckOfCard = () => { 
   for(let j = 0; j < suit.length; j++) {
     for (let i = 0; i < rank.length; i++) {
@@ -21,5 +21,11 @@ const createDeckOfCard = () => {
       }
       deck.push(card)
     }
+  }
+  for (i = 0; i < deck.length; i++) {
+    let j = Math.floor(Math.random() * i)
+    let temp = deck[i]
+    deck[i] = deck[j]
+    deck[j] = temp
   }
 }
