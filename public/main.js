2c312103d502a9c83481743afbfc11fff8c59d61
Michael Solone <michaelsolone@gmail.com>
2018-08-02 15:11:58 -0400

    Deal button deals 2 face up cards to player and 2 unseen cards to dealer

--- public/main.js
+++ public/main.js
@@ -9,6 +9,8 @@ const deck = []
 const suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 const rank = ['Ace', '2','3','4','5','6','7','8','9','10','Jack','Queen','King']
 const cardValue = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+playerHand = []
+dealerHand = []
 
 // Creates an Array of 52 objects with rank, suits, and card values
 const createDeckOfCard = () => { 
@@ -29,3 +31,40 @@ const createDeckOfCard = () => {
     deck[j] = temp
   }
 }
+
+const dealCards = () => {
+  for (i = 1; i < 5; i++) {
+    if (i % 2 == 0) {
+      // If index is even add to players hand
+      playerHand.push(deck[deck.length - 1])
+      deck.pop()
+      } else {
+        // if index is odd add to dealers hand
+        dealerHand.push(deck[deck.length - 1])
+        deck.pop()
+      }
+  }
+  let newLi = document.createElement('li')
+  card = playerHand[0]
+  newLi.textContent = card.rank + ' of ' + card.suit
+  document.querySelector('.player-hand').appendChild(newLi)
+  newLi = document.createElement('li')
+  card = playerHand[1]
+  newLi.textContent = card.rank + ' of ' + card.suit
+  document.querySelector('.player-hand').appendChild(newLi)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('.deal').addEventListener('click', dealCards)
+
+document.addEventListener('DOMContentLoaded', createDeckOfCard)
