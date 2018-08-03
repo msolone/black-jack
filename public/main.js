853b396721126fc441f82232c852bae2c2c0dae3
Michael Solone <michaelsolone@gmail.com>
2018-08-02 22:36:38 -0400

    Fully functional black jack game, still play again option and styling

--- public/main.js
+++ public/main.js
@@ -50,51 +50,91 @@ const dealCards = () => {
         deck.pop()
       }
   }
+  // Reveals first card in players hand
   let newLi = document.createElement('li')
   card = playerHand[0]
   newLi.textContent = card.rank + ' of ' + card.suit
   document.querySelector('.player-hand').appendChild(newLi)
+  // Reveals second card in players hand
   newLi = document.createElement('li')
   card = playerHand[1]
   newLi.textContent = card.rank + ' of ' + card.suit
   document.querySelector('.player-hand').appendChild(newLi)
+  // Displays players current score
   let score = playerHand[0].value + playerHand[1].value
   document.querySelector('.current-score').textContent = score
  
 }
 
-// const calculateScore = () => {
-//   for (i = 0; i < playerHand.length; i++) {
-//     playerOneScore += playerHand[i].value 
-//   }
-//   console.log(playerOneScore)   
-// }
-
-// // dealers score 
-// dealerScore = dealerHand[0].value + dealerHand[1].value
-
-
 const hitMe = () => {
+  // Adds cards to player hand, eliminates top card from deck
   playerHand.push(deck[deck.length - 1])
   deck.pop()
+  // determines player score
   playerOneScore = 0
   for (i = 0; i < playerHand.length; i++) {
     playerOneScore += playerHand[i].value 
-  }
-  console.log(playerOneScore) 
-
+    }
+  // Reveals card dealt to player  
   let newLi = document.createElement('li')
   card = playerHand[playerHand.length -1]
   newLi.textContent = card.rank + ' of ' + card.suit
   document.querySelector('.player-hand').appendChild(newLi)
-
+  // Updates the players current score
   document.querySelector('.current-score').textContent = playerOneScore
-  
+  // Ends game if players score goes over 21
+  if (playerOneScore > 21 ) {
+    document.querySelector('.declare-winner-banner').textContent = 'Bust, Dealer Wins!'
+  }
 
 }
 
 
+const standAndPresent = () => {
+  // Ree-establish players score
+  playerOneScore = 0
+  for (i = 0; i < playerHand.length; i++) {
+    playerOneScore += playerHand[i].value 
+    }
+  dealerScore = dealerHand[0].value + dealerHand[1].value
+  
+  // Add cards to Dealers hand until score is greater then players
+  while (dealerScore < playerOneScore  && dealerScore <= 21) {
+    dealerScore = 0
+    dealerHand.push(deck[deck.length - 1])
+    deck.pop() 
+    for (let i = 0; i < dealerHand.length; i++) {
+      dealerScore += dealerHand[i].value 
+    }
 
+  }
+  // Reveal dealer cards 
+  for (let i = 0; i < dealerHand.length; i ++) {
+    const newLi = document.createElement('li')
+    card = dealerHand[i]
+    newLi.textContent = card.rank + ' of ' + card.suit
+    document.querySelector('.dealer-hand').appendChild(newLi)
+    document.querySelector('.current-score2').textContent = dealerScore
+  }
+  // Compares scores and announces winner
+  if (dealerScore > 21) {
+    document.querySelector('.declare-winner-banner').textContent = 'Dealer Bust, You Win!'
+    } else {
+      if (dealerScore === playerOneScore) {
+        document.querySelector('.declare-winner-banner').textContent = 'Tie, House Rules, Dealer Wins'
+      } else if (dealerScore > playerOneScore) {
+        document.querySelector('.declare-winner-banner').textContent = 'Dealer Wins'
+        } else {
+          document.querySelector('.declare-winner-banner').textContent = 'You Win'
+        }
+      }
+    
+
+  }
+  
+  const playAgain = () => {
+    console.log('this works')
+  }
 
 
 
@@ -108,3 +148,5 @@ document.querySelector('.deal').addEventListener('click', dealCards)
 document.querySelector('.hit').addEventListener('click', hitMe)
 
 document.querySelector('.stand').addEventListener('click', standAndPresent)
+
+document.querySelector('.play-again').addEventListener('click', playAgain)
